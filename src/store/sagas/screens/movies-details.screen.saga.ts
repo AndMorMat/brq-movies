1c3408435a709e6c431be501152a3c19e3c8ba27
Andre Matos <andre_matos13@hotmail.com>
2023-08-22 17:56:32 -0300

    Update movies-details.screen.saga.ts

--- src/store/sagas/screens/movies-details.screen.saga.ts
+++ src/store/sagas/screens/movies-details.screen.saga.ts
@@ -1,6 +1,9 @@
 import { takeLatest, put } from 'redux-saga/effects'
 import { TMDBService } from '~services/tmdb.service'
-import { FETCH_MOVIES_DETAILS_ACTION, fetchMoviesDetailsSuccessAction } from '~actions/screens/movies-details.screen.action'
+import { 
+  FETCH_MOVIES_DETAILS_ACTION, 
+  fetchMoviesDetailsSuccessAction 
+} from '~actions/screens/movies-details.screen.action'
 
 function* handleMoviesDetailsAction(action) {
   const { movieId } = action.payload
